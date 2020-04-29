1b9dd512c63f5641d12aa7f5c27b97b763513d1c
Jdowdy <Jdowdy9k@gamil.com>
2020-04-29 19:20:31 -0400

    fixed note posting without id

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,66 +7,51 @@ const outputPath = path.join(DB_DIR, "./db.json");
 
 let notesArray = notes_DB;
 
+
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
         res.json(notes_DB);
     });
 
+
     app.post("/api/notes", function (req, res) {
 
-        if (notesArray.length === 0) {
-            notesArray.push({
-                "text": "",
-                "text": "",
-                "id": 0
+        const ranId = Math.floor((Math.random() * 100) + 1);
+
+        let newNote =
+        {
+            id: ranId,
+            title: req.body.title,
+            text: req.body.text
+        };
+
+       
+        fs.readFile(outputPath, (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            notesArray.push(newNote)
+
+
+            fs.writeFile(outputPath, JSON.stringify(notesArray), function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+                res.send(notes_DB);
             });
-        }
-
-        if (notesArray[0].title === "") {
-            notesArray.shift();
-        }
-
-        notesArray.push(req.body);
-
-        let finishNote = JSON.stringify(notesArray, null, 2);
-
-        fs.writeFile(outputPath, JSON.stringify(notesArray), function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Success! You've saved a note!");
-            }
-            res.json(finishNote);
+         
         });
-
     });
-
-    app.delete("/api/notes/:id", function (req, res) {
-
-        var chosen = req.params.id;
-
-        console.log(chosen);
-
-        for (var i = 0; i < notesArray.length; i++) {
-            if (chosen === notesArray[i].id) {
-                notesArray.splice(i, 1);
-
-                let finishNote = JSON.stringify(notesArray, null, 2);
-                fs.writeFile(outputPath, finishNote, function (err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        res.json(finishNote);
-                        console.log("Success! You've deleted a note!");
-                    }
-
-                });
-
-            }
-        }
-
-
-    });
-
-
-};
+    app.delete("/api/notes/:id", (req, res) => {
+        var idChosen = req.params.id;
+        fs.readFile(outputPath, (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            var notesArr = JSON.parse(data);
+            var newNotesArr = notesArray.filter(notes => notes.id !== idChosen);
+            fs.writeFile(outputPath, JSON.stringify(newNotesArr), function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+                res.send(notes_DB);
+            });
+        })
+    })
+}
